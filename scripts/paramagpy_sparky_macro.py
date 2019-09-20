85142c82cc6b63f744588663465369a2e02c69d5
henryorton <henryorton95@gmail.com>
2019-09-20 11:11:18 +1000

    sparky pcs script includes error calculation

--- scripts/paramagpy_sparky_macro.py
+++ scripts/paramagpy_sparky_macro.py
@@ -18,13 +18,22 @@ class ReadWritePCSFiles(tkutil.Dialog):
 		self.session = session
 		self.grid_widgets = []
 
-		tkutil.Dialog.__init__(self, session.tk, 'Fit Delta-Chi Tensor to PCS')
+		tkutil.Dialog.__init__(self, session.tk, 'Read and write PCS values')
+		acqu_template = [("T{}-:".format(i+1), "0.0", 6) for i in range(3)]
 
-		self.dia_menu = sputil.spectrum_menu(session, self.top, 'Diamagnetic Spectrum:  ')
+		self.dia_menu = sputil.spectrum_menu(session, self.top, 
+			'Diamagnetic Spectrum:  ')
 		self.dia_menu.frame.pack(side = 'top', anchor = 'w')
+		self.dia_menu.add_callback(self.dia_spec_change)
+		self.dia_acqu = tkutil.entry_row(self.top, "Acqu Time:", *acqu_template)
+		self.dia_acqu.frame.pack(side = 'top', anchor = 'w')
 
-		self.para_menu = sputil.spectrum_menu(session, self.top, 'Paramagnetic Spectrum: ')
+		self.para_menu = sputil.spectrum_menu(session, self.top, 
+			'Paramagnetic Spectrum: ')
 		self.para_menu.frame.pack(side = 'top', anchor = 'w')
+		self.para_menu.add_callback(self.para_spec_change)
+		self.para_acqu = tkutil.entry_row(self.top, "Acqu Time:", *acqu_template)
+		self.para_acqu.frame.pack(side = 'top', anchor = 'w')
 
 		self.npc_write_path = tkutil.file_field(self.top, 'PCS save file: ', '.npc', save=1)
 		self.npc_write_path.frame.pack(side = 'top', anchor = 'e')
@@ -45,6 +54,89 @@ class ReadWritePCSFiles(tkutil.Dialog):
 			('Close', self.close_cb))
 		br.frame.pack(side = 'top', anchor = 'w')
 
+		self.dia_spec_change()
+		self.para_spec_change()
+
+	@staticmethod
+	def parse_bruker_procs(fileName):
+		with open(fileName) as f:
+			data = {}
+			for line in f:
+				if line.startswith('##$'):
+					key, value = line.split("=")
+					try:
+						value = float(value)
+					except ValueError:
+						pass
+					data[key.replace('##$','')] = value
+			return data
+
+
+	def dia_spec_change(self, *args):
+		spec = self.dia_menu.spectrum()
+		if not spec:
+			return
+
+		taqs = []
+		for i in range(3):
+			filename = "proc{}s".format(i+1).replace("1","")
+			pathname = os.path.join(os.path.dirname(spec.data_path), filename)
+			if os.path.exists(pathname):
+				try:
+					dic = self.parse_bruker_procs(pathname)
+					taq = 0.5 * (dic['TDeff'] / dic['SW_p']) * (dic['STSI'] / dic['FTSIZE'])
+					taqs.append(taq)
+				except KeyError:
+					pass
+
+		if len(taqs) != spec.dimension:
+			taqs = [0.05]*spec.dimension
+		taqs = taqs[::-1]
+
+		for i in range(3):
+			e = self.dia_acqu.entries[i]
+			v = self.dia_acqu.variables[i]
+			if i<spec.dimension:
+				e.entry.config(state='normal')
+				e.label['text'] = "T{}-{}:".format(i+1, spec.nuclei[i])
+				v.set("{:6.4f}".format(taqs[i]))
+			else:
+				e.entry.config(state='disabled')
+				v.set("0.0")
+
+
+	def para_spec_change(self, *args):
+		spec = self.para_menu.spectrum()
+		if not spec:
+			return
+
+		taqs = []
+		for i in range(3):
+			filename = "proc{}s".format(i+1).replace("1","")
+			pathname = os.path.join(os.path.dirname(spec.data_path), filename)
+			if os.path.exists(pathname):
+				try:
+					dic = self.parse_bruker_procs(pathname)
+					taq = 0.5 * (dic['TDeff'] / dic['SW_p']) * (dic['STSI'] / dic['FTSIZE'])
+					taqs.append(taq)
+				except KeyError:
+					pass
+
+		if len(taqs) != spec.dimension:
+			taqs = [0.05]*spec.dimension
+		taqs = taqs[::-1]
+
+		for i in range(3):
+			e = self.para_acqu.entries[i]
+			v = self.para_acqu.variables[i]
+			if i<spec.dimension:
+				e.entry.config(state='normal')
+				e.label['text'] = "T{}-{}:".format(i+1, spec.nuclei[i])
+				v.set("{:6.4f}".format(taqs[i]))
+			else:
+				e.entry.config(state='disabled')
+				v.set("0.0")
+
 
 	@staticmethod
 	def unpack_peaks(peakList):
@@ -52,7 +144,7 @@ class ReadWritePCSFiles(tkutil.Dialog):
 		for peak in peakList:
 			if peak.note == "PCS_backcalc":
 				continue
-			peaks[peak.assignment] = peak.frequency
+			peaks[peak.assignment] = peak
 		return peaks
 
 	def write_npc_cb(self):
@@ -69,24 +161,35 @@ class ReadWritePCSFiles(tkutil.Dialog):
 
 		pcs = {}
 		for assig in common:
-			new_freq = []
-			for d, p in zip(dia[assig], para[assig]):
-				new_freq.append(p-d)
-			pcs[assig] = tuple(new_freq)
+			pd = dia[assig]
+			pp = para[assig]
+			freq = [p-d for d, p in zip(pd.frequency, pp.frequency)]
+			edia = []
+			for i in range(diaSpec.dimension):
+				taq = float(self.dia_acqu.variables[i].get())
+				tmp = ((2./3.) * diaSpec.noise) / (pd.data_height * taq)
+				edia.append(tmp)
+
+			epara = []
+			for i in range(paraSpec.dimension):
+				taq = float(self.para_acqu.variables[i].get())
+				tmp = ((2./3.) * paraSpec.noise) / (pp.data_height * taq)
+				epara.append(tmp)
+
+			error = [(d**2 + p**2)**0.5 for d, p in zip(edia, epara)]
+			pcs[assig] = tuple(freq), tuple(error)
 
 		out = {}
 		for assig in pcs:
-			for (tmp, atom), value in zip(sputil.parse_assignment(assig), pcs[assig]):
-				m = re.findall(r"\d+", tmp)
-				if m:
-					seq = int(m[0])
-					out[(seq, atom)] = value
+			for (tmp, atom), value, error in zip(sputil.parse_assignment(assig), *pcs[assig]):
+				seq = int(re.findall(r"\d+", tmp)[0])
+				out[(seq, atom)] = value, error
 
 		with open(fileName, 'w') as f:
 			for key in sorted(out):
 				seq, atom = key
-				line = "{0:<3d} {1:<3s} {2:6.3f} 0.000\n".format(
-					seq, atom, out[key])
+				line = "{0:<3d} {1:<3s} {2:6.3f} {3:6.3f}\n".format(
+					seq, atom, out[key][0], out[key][1])
 				f.write(line)
 
 		self.message['text'] = "PCS values written to file: \n{}".format(fileName)
